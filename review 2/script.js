fb88f2669b00af1c4824a83c8d2091cc4a7cb5c7
Mr. Gaurav Kumar Raj <mrgauravfactician2456@gmail.com>
2025-01-21 13:34:52 +0530

    Delete review 2 directory

--- review 2/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    if (username === '' || password === '') {
-        alert('Both fields are required!');
-        event.preventDefault();
-    }
-});
